e7e51d0272243117262216a7e9257e078af66cf8
Justin-JtZ <46854715+Justin-JtZ@users.noreply.github.com>
2019-04-10 18:50:01 -0400

    Map completed

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+     void switchToStart()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+
+    void switchToGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    void switchToOverview()
+    {
+        SceneManager.LoadScene("Overview");
+    }
+
+    void switchToControls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+}
